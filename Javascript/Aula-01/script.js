74b43b0b1d558cfa537e3f00ea8c45707433b729
MathMartiin <matheusmartinmota0@gmail.com>
2025-01-31 12:19:10 -0300

    Commit da aula 1

---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,23 @@
+//console.log("Ola Mundo")
+//exemplo de alert
+//alert("Ola Mundo 1")
+//exemplo de confirm
+//var teste = confirm("Ola Mundo 2")
+//exebindo a opção escolhida pelo usuario
+//console.log(teste)
+
+
+//exemplo de prompt
+//var testeprompt = prompt("Digite seu nome ")
+
+//console.assert(testeprompt)
+//alert(testeprompt)
+
+
+//tentativa de adição
+var numero1 = Number.parseInt(prompt("Digite o primeiro número"))
+var numero2 = Number.parseInt(prompt("Digite o segundo número"))
+//verificando o tipo da variavel
+console.log(typeof(numero1))
+alert(numero1 + numero2);
+
