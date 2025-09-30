c1bb6e91a6b047eb2f0a5388312d43fb0378d35e
MathMartiin <matheusmartinmota0@gmail.com>
2025-09-30 16:23:55 -0300

    Commit da aula de Bootstrap

--- Projeto-Produtos/client/selectClients.php
+++
@@ -1,7 +0,0 @@
-<?php
-        include "conn.php";
-        $dados = $db->query("SELECT * FROM clientes");
-        $dados->execute();
-        $qnt = $dados->rowCount();
-        $result = $dados->fetchAll(PDO::FETCH_ASSOC);
-?>
--- Projeto-Produtos/product/selectProducts.php
+++
@@ -1,7 +0,0 @@
-<?php
-        include "./conn.php";
-        $dados = $db->query("SELECT * FROM produtos");
-        $dados->execute();
-        $qnt = $dados->rowCount();
-        $result = $dados->fetchAll(PDO::FETCH_ASSOC);
-?>
